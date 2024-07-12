073c57f1524bda2779394fbb5dfff851e37e96df
7irelo <tirelo.eric@gmail.com>
2024-07-12 14:03:06 +0200

    Buddy

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace study_gymon_asp_api;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
